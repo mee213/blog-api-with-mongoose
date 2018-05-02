7630f4e0716007534034a31f7e4b71976be04a5f
mee213 <evans.marlo@gmail.com>
2018-05-02 13:39:31 -0500

    added mocha, chai, and chai-http, as well as passing integration tests for all 4 API endpoints

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -81,7 +81,7 @@ router.put('/:id', jsonParser, (req, res) => {
     author: req.body.author,
     publishDate: req.body.publishDate
   });
-  res.status(204).end();
+  res.status(200).json(updatedItem);
 })
 
 module.exports = router;
